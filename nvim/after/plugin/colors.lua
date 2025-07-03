a30eda8a4e042f825c18843149aae0171ce770b3
Neutrino <neutrino249@gmail.com>
2025-07-04 00:11:42 +0530

    nvim tokyonight theme

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,10 @@
 function PaintVim(color)
-	color = color or "catppuccin"
+	color = color or "tokyonight-moon"
 	vim.cmd.colorscheme(color)
+	vim.cmd([[highlight SignColumn guibg=none guifg=#88c0d0]])
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
-PaintVim();
+PaintVim()
