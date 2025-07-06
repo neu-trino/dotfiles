4c3ba2cb455a11e07426d8b0f9157b5f465de825
Neutrino <neutrino249@gmail.com>
2025-07-06 19:20:23 +0530

    chargemode script refactor

--- sway/core/scripts/charge-mode.sh
+++ sway/core/scripts/charge-mode.sh
@@ -7,16 +7,20 @@ CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu -p "Select")
 case "$CHOICE" in
     "Max lifespan (60%)")
         footclient --app-id floating_shell --window-size-chars 82x25 -e bash -c "sudo ~/.config/sway/core/scripts/charge-limiter.sh -m > ~/.local/state/charge-limiter.state"
+        notify-send "Charge Mode" "$(cat ~/.local/state/charge-limiter.state)"
         ;;
     "Balanced (80%)")
         footclient --app-id floating_shell --window-size-chars 82x25 -e bash -c "sudo ~/.config/sway/core/scripts/charge-limiter.sh -b > ~/.local/state/charge-limiter.state"
+        notify-send "Charge Mode" "$(cat ~/.local/state/charge-limiter.state)"
         ;;
     "Full (100%)")
         footclient --app-id floating_shell --window-size-chars 82x25 -e bash -c "sudo ~/.config/sway/core/scripts/charge-limiter.sh -f > ~/.local/state/charge-limiter.state"
+        notify-send "Charge Mode" "$(cat ~/.local/state/charge-limiter.state)"
         ;;
     *)
-        notify-send "Charge Mode" "Invalid option"
+        if [ -n "$CHOICE" ]; then
+         notify-send "Charge Mode" "Invalid option"
+        fi
         ;;
 esac
 
-notify-send "Charge Mode" "$(cat ~/.local/state/charge-limiter.state)"
