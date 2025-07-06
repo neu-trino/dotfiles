6d3311c79675e202810f6d6c1e402e40d1b8b5b0
Neutrino <neutrino249@gmail.com>
2025-07-06 18:50:55 +0530

    nvim transparent hightlights

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,10 +1,19 @@
 function PaintVim(color)
-	color = color or "tokyonight-moon"
 	vim.cmd.colorscheme(color)
-	vim.cmd([[highlight SignColumn guibg=none guifg=#88c0d0]])
+
+	local statusline_hl = vim.api.nvim_get_hl(0, { name = "StatusLine", link = false })
+	local float_title_hl = vim.api.nvim_get_hl(0, { name = "FloatTitle", link = false })
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+	vim.api.nvim_set_hl(0, "WinBar", { bg = "NONE", fg = statusline_hl.fg })
+	vim.api.nvim_set_hl(0, "WinBarNC", { bg = "NONE", fg = statusline_hl.fg })
+	vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE", fg = statusline_hl.fg })
+	vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE", fg = statusline_hl.fg })
+	vim.api.nvim_set_hl(0, "FloatTitle", { bg = "NONE", fg = float_title_hl.fg })
+	vim.api.nvim_set_hl(0, "FloatBorder", { bg = "NONE", fg = float_title_hl.fg })
 end
 
-PaintVim()
+PaintVim("tokyonight-moon")
